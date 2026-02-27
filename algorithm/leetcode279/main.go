ee93c28977575a9c277f202150f15ed746bf5863
jimoruchen <1007896918@qq.com>
2026-02-27 15:52:45 +0800

    2026.2.27 leetcode279

---
+++ algorithm/leetcode279/main.go
@@ -0,0 +1,15 @@
+package main
+
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for i := 1; i <= n; i++ {
+		dp[i] = n + 1
+	}
+	for i := 1; i*i <= n; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
+		}
+	}
+	return dp[n]
+}

5bdc28e686b3be4ac7bb4f3286ac60859f5476e4
jimoruchen <1007896918@qq.com>
2026-03-17 10:59:52 +0800

    2026.3.17 leetcode05

--- algorithm/leetcode05/main.go
+++ algorithm/leetcode05/main.go
@@ -19,3 +19,23 @@ func longestPalindrome(s string) string {
 	}
 	return ans
 }
+
+func longestPalindrome1(s string) string {
+	var ans string
+	n := 0
+	var expand = func(left, right int) {
+		for left >= 0 && right < len(s) && s[left] == s[right] {
+			if right-left+1 > n {
+				ans = s[left : right+1]
+				n = right - left + 1
+			}
+			left--
+			right++
+		}
+	}
+	for i := 0; i < len(s); i++ {
+		expand(i, i)
+		expand(i, i+1)
+	}
+	return ans
+}

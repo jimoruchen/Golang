545f59d507ec6d39e3c1eaf780d0445dfbf103d0
jimoruchen <1007896918@qq.com>
2026-03-17 10:55:18 +0800

    2026.3.17 leetcode53

--- algorithm/leetcode53/main.go
+++ algorithm/leetcode53/main.go
@@ -15,6 +15,32 @@ func maxSubArray(nums []int) int {
 	return maxSum
 }
 
+func maxSubArray1(nums []int) int {
+	maxSum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		maxSum = max(maxSum, nums[i])
+	}
+	return maxSum
+}
+
+func maxSubArray2(nums []int) int {
+	maxSum := nums[0]
+	dp := make([]int, len(nums)) //
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = dp[i-1] + nums[i]
+		} else {
+			dp[i] = nums[i]
+		}
+		maxSum = max(maxSum, dp[i])
+	}
+	return maxSum
+}
+
 func main() {
 	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 }

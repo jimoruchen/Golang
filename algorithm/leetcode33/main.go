c709140090f8d7dc761360da4ef361b505de4eef
jimoruchen <1007896918@qq.com>
2026-03-17 11:04:04 +0800

    2026.3.17 leetcode33

--- algorithm/leetcode33/main.go
+++ algorithm/leetcode33/main.go
@@ -32,3 +32,27 @@ func search(nums []int, target int) int {
 	}
 	return -1
 }
+
+func search1(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if target == nums[mid] {
+			return mid
+		}
+		if nums[mid] >= nums[left] {
+			if target < nums[mid] && target >= nums[left] {
+				right = mid
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right-1] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+	}
+	return -1
+}

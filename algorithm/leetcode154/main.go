f93a4ba2551a535c78317bc029c3fe830b0000a1
jimoruchen <1007896918@qq.com>
2026-03-17 11:05:01 +0800

    2026.3.17 leetcode154

--- algorithm/leetcode154/main.go
+++ algorithm/leetcode154/main.go
@@ -14,3 +14,18 @@ func findMin(nums []int) int {
 	}
 	return nums[left]
 }
+
+func findMin1(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else if nums[mid] == nums[right] {
+			right--
+		} else {
+			right = mid
+		}
+	}
+	return nums[left]
+}

1d375e3c264439e7ae33365031597421a9e1bb2a
jimoruchen <1007896918@qq.com>
2026-03-17 10:54:37 +0800

    2026.3.17 leetcode15

--- algorithm/leetcode15/main.go
+++ algorithm/leetcode15/main.go
@@ -36,6 +36,37 @@ func threeSum(nums []int) [][]int {
 	return ans
 }
 
+func threeSum1(nums []int) [][]int {
+	sort.Ints(nums)
+	n := len(nums)
+	var ans [][]int
+	for left := 0; left < n-2; left++ {
+		if left > 0 && nums[left] == nums[left-1] {
+			continue
+		}
+		j := left + 1
+		right := n - 1
+		for j < right {
+			if nums[left]+nums[j]+nums[right] == 0 {
+				ans = append(ans, []int{nums[left], nums[j], nums[right]})
+				for j < right && nums[j] == nums[j+1] {
+					j++
+				}
+				for j < right && nums[right] == nums[right-1] {
+					right--
+				}
+				j++
+				right--
+			} else if nums[left]+nums[j]+nums[right] > 0 {
+				right--
+			} else {
+				j++
+			}
+		}
+	}
+	return ans
+}
+
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
 	ans := threeSum(nums)

1726a221715c865a25008bb198a1a867fa399c3b
jimoruchen <1007896918@qq.com>
2026-03-24 09:16:31 +0800

    2026.3.24 leetcode199

--- algorithm/leetcode199/main.go
+++ algorithm/leetcode199/main.go
@@ -30,3 +30,28 @@ func rightSideView(root *TreeNode) []int {
 	}
 	return ans
 }
+
+func rightSideView1(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	queue := []*TreeNode{root}
+	var ans []int
+	for len(queue) > 0 {
+		length := len(queue)
+		for i := 0; i < length; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i == length-1 {
+				ans = append(ans, node.Val)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return ans
+}

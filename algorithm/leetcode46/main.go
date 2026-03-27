65646ae846eb907742f3171cde1aadc3f8349197
jimoruchen <1007896918@qq.com>
2026-03-27 10:01:20 +0800

    2026.3.27 leetcode46

--- algorithm/leetcode46/main.go
+++ algorithm/leetcode46/main.go
@@ -29,6 +29,33 @@ func permute(nums []int) [][]int {
 	return result
 }
 
+func permute1(nums []int) [][]int {
+	var resulit [][]int
+	var path []int
+	visited := make([]bool, len(nums))
+	var backtracking func()
+	backtracking = func() {
+		if len(path) == len(nums) {
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			resulit = append(resulit, tmp)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if visited[i] {
+				continue
+			}
+			path = append(path, nums[i])
+			visited[i] = true
+			backtracking()
+			visited[i] = false
+			path = path[:len(path)-1]
+		}
+	}
+	backtracking()
+	return resulit
+}
+
 func main() {
 	nums := []int{1, 2, 3, 4}
 	fmt.Println(permute(nums))
